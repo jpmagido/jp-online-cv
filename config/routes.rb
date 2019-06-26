28289651111d2c7ad56e5342f4b001e8b5681337
Jean-Philippe MAGIDO <jean-philippemagido@jean-philippes-imac.home>
2019-06-26 17:25:45 +0200

    Create landing page

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  get 'static_pages/hello'
+  root 'static_pages#hello'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
