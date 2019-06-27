b461577dc57bd0032c6a897b01dfa42d53b82a80
Jean-Philippe MAGIDO <jean-philippemagido@jean-philippes-imac.home>
2019-06-27 14:29:40 +0200

    fix seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,8 +14,8 @@ def destroy
 end
 
 def projects
-	Project.create(name: "Antonius", nature: "Website Transformation", level: 5, url: "https://boutique-ha-app.herokuapp.com", client: "Antonius Paris", livraison: 2019-06-04 , duration: 1, description: "Transformation d'un site existant en site marchand. Intégration d'un système de paiement et logistique")
-	Project.create(name: "Gracie Barra", nature: "Website Creation", level: 6, url: "http://gracie-ha-app.herokuapp.com", client: "GracieBarra Paris", livraison: 2019-06-26, duration: 1, description: "Refonte totale du site Web Front & Back. Intégration d'un système User & Admin + système de paiement")
+	Project.create(name: "Antonius", nature: "Website Transformation", level: 5, url: "https://boutique-ha-app.herokuapp.com", client: "Antonius Paris", livraison: "04-06-2019" , duration: 1, description: "Transformation d'un site existant en site marchand. Intégration d'un système de paiement et logistique")
+	Project.create(name: "Gracie Barra", nature: "Website Creation", level: 6, url: "http://gracie-ha-app.herokuapp.com", client: "GracieBarra Paris", livraison: "26-06-2019", duration: 1, description: "Refonte totale du site Web Front & Back. Intégration d'un système User & Admin + système de paiement")
 end
 
 def perform
