8630e303d6ede0a4edebd9c4977dd9b3761c2ff1
Jean-Philippe MAGIDO <jean-philippemagido@jean-philippes-imac.home>
2019-06-27 14:19:45 +0200

    Create seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+#Project.create(name: , nature: , level: , url: , client: , livraison: , duration: , description: )
+
+
+def destroy
+	Project.destroy_all
+end
+
+def projects
+	Project.create(name: "Antonius", nature: "Website Transformation", level: 5, url: "https://boutique-ha-app.herokuapp.com", client: "Antonius Paris", livraison: 2019-06-04 , duration: 1, description: "Transformation d'un site existant en site marchand. Intégration d'un système de paiement et logistique")
+	Project.create(name: "Gracie Barra", nature: "Website Creation", level: 6, url: "http://gracie-ha-app.herokuapp.com", client: "GracieBarra Paris", livraison: 2019-06-26, duration: 1, description: "Refonte totale du site Web Front & Back. Intégration d'un système User & Admin + système de paiement")
+end
+
+def perform
+	destroy
+	projects
+end
+
+perform
