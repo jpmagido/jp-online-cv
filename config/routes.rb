7f6ce0d5bcdea4ad6a27a8c78c01378c465ef10a
jpmagido <jpmagido@me.com>
2021-03-12 16:42:45 +0100

    refactor + start second article

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,6 @@
 Rails.application.routes.draw do
+
+  get resources :lessons, only: [:show]
   get 'blog_pages/home'
-  get 'blog_pages/lesson_1'
-  
-  get 'project/index'
-  
-  get 'static_pages/hello'
-  
   root 'static_pages#hello'
-  
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -8,59 +8,71 @@
 
 #Project.create(name: , nature: , level: , url: , client: , livraison: , duration: , description: )
 
-
 def destroy
-	Project.destroy_all
-	Lesson.destroy_all
+  Project.destroy_all
+  Lesson.destroy_all
 end
 
 def projects
-	Project.create(name: "Antonius", 
-		nature: "Website Transformation", 
-		level: 2, 
-		url: "http://antonius.paris", 
-		github_url: 'https://github.com/jpmagido',
-		client: "Antonius Paris", 
-		livraison: "04-06-2019" , 
-		duration: 3, 
-		description: "Transformation d'un site existant en site marchand. Intégration d'un système de paiement et logistique")
+  Project.create!(
+    name: "Antonius",
+    nature: "Website Transformation",
+    level: 2,
+    url: "http://antonius.paris",
+    github_url: 'https://github.com/jpmagido',
+    client: "Antonius Paris",
+    livraison: "04-06-2019",
+    duration: 3,
+    description: "Transformation d'un site existant en site marchand. Intégration d'un système de paiement et logistique"
+  )
 
-	Project.create(name: "Gracie Barra", 
-		nature: "Website Creation", 
-		level: 3, 
-		url: "http://gracie-ha-app.herokuapp.com", 
-		github_url: 'https://github.com/jpmagido/Projet_Gracie',
-		client: "GracieBarra Paris", 
-		livraison: "26-06-2019", 
-		duration: 4, 
-		description: "Refonte totale du site Web Front & Back. Intégration d'un système User & Admin + système de paiement")
-	
-	Project.create(name: "Top Educs", 
-		nature: "Website Creation", 
-		level: 3, 
-		url: "http://gestion-topeduc.herokuapp.com/", 
-		github_url: 'https://github.com/jpmagido/topeduc_bis',
-		client: "Top Educs Corporation", 
-		livraison: nil, 
-		duration: 2, 
-		description: "Refonte Front. Intégration d'un système de gestion du recrutement")
+  Project.create!(
+    name: "Gracie Barra",
+    nature: "Website Creation",
+    level: 3,
+    url: "http://gracie-ha-app.herokuapp.com",
+    github_url: 'https://github.com/jpmagido/Projet_Gracie',
+    client: "GracieBarra Paris",
+    livraison: "26-06-2019",
+    duration: 4,
+    description: "Refonte totale du site Web Front & Back. Intégration d'un système User & Admin + système de paiement"
+  )
+
+  Project.create!(
+    name: "Top Educs",
+    nature: "Website Creation",
+    level: 3,
+    url: "http://gestion-topeduc.herokuapp.com/",
+    github_url: 'https://github.com/jpmagido/topeduc_bis',
+    client: "Top Educs Corporation",
+    livraison: nil,
+    duration: 2,
+    description: "Refonte Front. Intégration d'un système de gestion du recrutement"
+  )
 end
 
 def lessons
+  Lesson.create!(
+    title: 'From Bootcamp to Dev',
+    description: 'Mon parcours après le Bootcamp. La transition avec le CDI',
+    url: "lesson_1",
+    number: 1
+  )
 
-	Lesson.create(title: 'From Bootcamp to Dev', 
-		description: 'Mon parcours après le Bootcamp. La transition avec le CDI', 
-		url: "lesson_1", 
-		number: 1
-	)
-	
+  Lesson.create!(
+    title: 'Am I a Developer ?',
+    description: 'Mais, mais... Ceci est un CDI ? Graal ou Chimère',
+    url: "lesson_2",
+    number: 2
+  )
+
 end
 
 def perform
-	destroy
-	projects
-	lessons
-	puts "Seed done"
+  destroy
+  projects
+  lessons
+  puts "Seed done"
 end
 
 perform
